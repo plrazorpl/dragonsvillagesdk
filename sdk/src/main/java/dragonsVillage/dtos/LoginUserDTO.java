3f5cb489c0960bafcfcca122517ae0c7c4b43a88
Lukasz Reszczynski <plrazorpl@o2.pl>
2014-12-21 00:44:32 +0100

    DV-V0.0.2 Updated LoginUserDTO

--- sdk/src/main/java/dragonsVillage/dtos/LoginUserDTO.java
+++ sdk/src/main/java/dragonsVillage/dtos/LoginUserDTO.java
@@ -21,4 +21,24 @@ public class LoginUserDTO implements Serializable {
     public void setLogin(String login) {
         this.login = login;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LoginUserDTO that = (LoginUserDTO) o;
+
+        if (id != that.id) return false;
+        if (login != null ? !login.equals(that.login) : that.login != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        return result;
+    }
 }
