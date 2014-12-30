6e7b672a4e8c85a3d1d334d358ded3832a988d44
Lukasz Reszczynski <plrazorpl@o2.pl>
2014-12-30 17:47:09 +0100

    DV-V0.2.0 Added equals dragonDTO

--- sdk/src/main/java/dragonsVillage/dtos/DragonDTO.java
+++ sdk/src/main/java/dragonsVillage/dtos/DragonDTO.java
@@ -108,4 +108,40 @@ public class DragonDTO extends ABaseDTO implements Serializable{
     public void setActualSharpY(int actualSharpY) {
         this.actualSharpY = actualSharpY;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DragonDTO dragonDTO = (DragonDTO) o;
+
+        if (actualSharpX != dragonDTO.actualSharpX) return false;
+        if (actualSharpY != dragonDTO.actualSharpY) return false;
+        if (positionX != dragonDTO.positionX) return false;
+        if (positionY != dragonDTO.positionY) return false;
+        if (dragonSkin != null ? !dragonSkin.equals(dragonDTO.dragonSkin) : dragonDTO.dragonSkin != null) return false;
+        if (dragonType != dragonDTO.dragonType) return false;
+        if (id != null ? !id.equals(dragonDTO.id) : dragonDTO.id != null) return false;
+        if (mapID != null ? !mapID.equals(dragonDTO.mapID) : dragonDTO.mapID != null) return false;
+        if (name != null ? !name.equals(dragonDTO.name) : dragonDTO.name != null) return false;
+        if (userID != null ? !userID.equals(dragonDTO.userID) : dragonDTO.userID != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + positionX;
+        result = 31 * result + positionY;
+        result = 31 * result + actualSharpX;
+        result = 31 * result + actualSharpY;
+        result = 31 * result + (userID != null ? userID.hashCode() : 0);
+        result = 31 * result + (mapID != null ? mapID.hashCode() : 0);
+        result = 31 * result + (dragonType != null ? dragonType.hashCode() : 0);
+        result = 31 * result + (dragonSkin != null ? dragonSkin.hashCode() : 0);
+        return result;
+    }
 }
