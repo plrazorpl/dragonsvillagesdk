4663039c2f874c3f62a2896eb836edc8aa7ff4c4
Lukasz Reszczynski <plrazorpl@o2.pl>
2015-01-10 14:49:26 +0100

    DV-V0.2.0 Added usage dragons graphic

--- sdk/src/main/java/dragonsVillage/Enums/EDragonType.java
+++ sdk/src/main/java/dragonsVillage/Enums/EDragonType.java
@@ -1,21 +1,29 @@
 package dragonsVillage.Enums;
 
 public enum EDragonType {
-    DEFAULT("dragons/default.png",1);
+    DEFAULT("dragons/default.png","dragons/default.png",1),
+    LITTLE_BLUE("dragons/small/dragonBlueLeft.png","dragons/small/dragonBlueRight.png",1);
 
-    private String path;
+
+    private String left;
+    private String right;
     private int size;
 
-    EDragonType(String path, int size) {
-        this.path = path;
+    EDragonType(String left, String right, int size) {
+        this.left = left;
+        this.right = right;
         this.size = size;
     }
 
-    public String getPath() {
-        return path;
+    public String getLeft() {
+        return left;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public String getRight() {
+        return right;
     }
 }
